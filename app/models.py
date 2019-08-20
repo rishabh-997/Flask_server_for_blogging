9c3351a983bdcfba6af881b7549547558b0770db
rishabh-997 <rishabh.agarwal997@gmail.com>
2019-08-21 02:24:08 +0530

    Added functionality to add about_me in database

--- app/models.py
+++ app/models.py
@@ -2,12 +2,15 @@ from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 class User(UserMixin , db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -22,6 +25,11 @@ class User(UserMixin , db.Model):
     def load_user(id):
         return User.query.get(int(id))
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -30,3 +38,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+# I hope you realize how useful it is to work with a migration framework.
+# Any users that were in the database are still there,
+# the migration framework surgically applies the changes in the migration script without destroying any data.
